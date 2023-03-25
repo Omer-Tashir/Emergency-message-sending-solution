d25adb466004b4dc5d94b37bbe3e43b5acbc1249
Omer-Tashir <omertashir1993@gmail.com>
2023-03-25 19:50:19 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAXr_Kt0pfMi_IOZG8GTGwGBe4WFUT-cL4",
+    authDomain: "emergency-msg-sending-solution.firebaseapp.com",
+    projectId: "emergency-msg-sending-solution",
+    storageBucket: "emergency-msg-sending-solution.appspot.com",
+    messagingSenderId: "776121382117",
+    appId: "1:776121382117:web:c54b75eb3914b9490d100c"
+  },
 };
